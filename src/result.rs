aab79444f8a94bb5e2682f1273300cda90a21bd6
Nathan <an.email.com>
2026-07-31 20:13:17 +0100

    polish

--- src/result.rs
+++ src/result.rs
@@ -16,12 +16,12 @@ pub enum TryReserveError {
 
 impl std::fmt::Display for TryReserveError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("memory allocation failed")?;
+        f.write_str("memory allocation failed because ")?;
         let reason = match self {
             TryReserveError::CapacityOverflow => {
-                " because the computed capacity exceeded the collection's maximum"
+                "the computed capacity exceeded the collection's maximum"
             }
-            TryReserveError::AllocError { .. } => " because the memory allocator returned an error",
+            TryReserveError::AllocError { .. } => "the memory allocator returned an error",
         };
         f.write_str(reason)
     }
