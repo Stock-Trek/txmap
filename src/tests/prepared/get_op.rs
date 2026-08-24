a00360c1dcb0bb053804458f895ab7d4c4edb8e0
Nathan <an.email.com>
2026-08-24 11:13:39 +0100

    update readme

--- src/tests/prepared/get_op.rs
+++ src/tests/prepared/get_op.rs
@@ -6,7 +6,7 @@ use crate::{
 #[test]
 fn param_get_many() {
     let map = map_alice(10);
-    let tx = map
+    let tx: GetTwoParamTx<'_, String, u64> = map
         .prepared_tx(&GetTwoParam::SCHEMA)
         .modify(GetTwoParam::a, |_k, v, _p, _s| *v += 0)
         .modify(GetTwoParam::b, |_k, v, _p, _s| *v += 0)
