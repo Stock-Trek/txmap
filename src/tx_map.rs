07daaa2ccaa81f9cdc7a40fb3bd67fc4fd1ae04d
Nathan <an.email.com>
2026-08-24 09:14:17 +0100

    revert helper functions

--- src/tx_map.rs
+++ src/tx_map.rs
@@ -4,11 +4,9 @@ use crate::{
     immediate::tx_builder::ImmediateTxBuilder,
     indexer::Indexer,
     iter::{Drain, Iter, Keys, Values},
-    key::TxKey,
-    lock_guards::LockGuards,
     lock_policies::{lock_policy::LockPolicy, mutex_policy::MutexPolicy},
     multi_shard_ops::MultiShardOps,
-    new_types::{BitMask, ShardCount, ShardIndex},
+    new_types::{ShardCount, ShardIndex},
     prepared::{
         schema::{TxKeys, TxSchema},
         tx_builder::{PreparedBuilderPhase, PreparedTxBuilder},
@@ -310,14 +308,6 @@ where
             guard.shrink_to(per_shard, |entry| self.indexer.hash(&entry.0).0);
             guards.push(guard);
         }
-    }
-    #[allow(dead_code)]
-    pub(crate) fn indexed_key(&self, key: K) -> TxKey<K> {
-        self.indexer.indexed_key(self.shard_count, key)
-    }
-    #[allow(dead_code)]
-    pub(crate) fn lock_guards(&self, read: BitMask, write: BitMask) -> LockGuards<'_, K, V, L> {
-        self.custodian.lock_guards(read, write)
     }
 }
 
